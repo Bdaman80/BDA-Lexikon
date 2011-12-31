baf4924c2f916e2cc7e0ce79b712959a6747554c
Bdaman80 <bdaman80@gmail.com>
2011-12-31 03:30:40 -0600

    Initial

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Sat Dec 31 03:18:23 CST 2011"
+#define LINUX_COMPILE_TIME "03:18:23"
+#define LINUX_COMPILE_BY "bdaman"
+#define LINUX_COMPILE_HOST "Height03"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "2.6.35.10BDA-Lexikon-2011-12-31"
